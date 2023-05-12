effdb637d6f7930dbb170e2079336e61c410be3d
今村悠聖 <yusei@imamurayuushounoMacBook-Air.local>
2023-05-12 11:09:09 +0900

    退避

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import { WelcomePage } from "./components/WelcomePage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<WelcomePage></WelcomePage>} />
-        <Route path="/home" element={<HomePage></HomePage>} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<WelcomePage></WelcomePage>} />
+    //     <Route path="/home" element={<HomePage></HomePage>} />
+    //   </Routes>
+    // </BrowserRouter>
+    <div className="border border-gray-400 rounded-2xl p-2 m-2 flex justify-around items-center">
+      <h1 className="text-3xl font-bold underline">Hello Tailwind CSS!</h1>
+      <p className="m-0 text-gray-400">Tailwind CSSです</p>
+      <button className="bg-gray-300 border-0 p-2 rounded-md hover:bg-gray-400 hover:text-white">
+        ボタン
+      </button>
+    </div>
   );
 }
 
