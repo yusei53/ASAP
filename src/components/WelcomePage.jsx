f0451da6a0ec96e583ce534a5fab5bd9acca6bac
今村悠聖 <yusei@imamurayuushounoMacBook-Air.local>
2023-06-06 12:01:38 +0900

    welcomepageにアニメーション追加

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,37 +1,55 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState, useEffect } from "react";
+import styled, { keyframes } from "styled-components";
 import { Link } from "react-router-dom";
 import { Header } from "./Header";
 
-export const WelcomePage = () => {
-  return (
-    <SBDiv>
-      <Header />
-      <Container>
-        <Text>どこにメールを送るのか選択してね！</Text>
-        <ButtonContainer>
-          <StyledLink to="/university">
-            <Button>大学(例:講義欠席)</Button>
-          </StyledLink>
-          <StyledLink to="/part-time">
-            <Button>バイト(例:バイト欠席、遅刻)</Button>
-          </StyledLink>
-          <StyledLink to="/company">
-            <Button>企業、会社(例:内定、インターン内定辞退)</Button>
-          </StyledLink>
-        </ButtonContainer>
-      </Container>
-    </SBDiv>
-  );
-};
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
 
-const SBDiv = styled.div`
+const fadeOut = keyframes`
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+`;
+
+const WelcomeContainer = styled.div`
+  animation: ${fadeIn} 1s ease;
+
+  /* Your existing styles */
   background-color: #e6f9f8;
   background-size: cover;
   background-position: center;
   height: 100vh;
   align-items: center;
   justify-content: center;
+`;
+
+const TextOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: #e6f9f8;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  animation: ${fadeOut} 1s ease 3s forwards;
+`;
+
+const Text = styled.div`
+  font-size: 48px;
+  font-weight: bold;
+  text-align: center;
 `;
 
 const Container = styled.div`
@@ -68,10 +86,45 @@ const Button = styled.button`
   }
 `;
 
-const Text = styled.div`
-  font-size: 36px;
-  font-weight: 600;
-  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.4);
-  margin-bottom: 20px;
-  margin-top: 5%;
-`;
+export const WelcomePage = () => {
+  const [showText, setShowText] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowText(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  return (
+    <WelcomeContainer>
+      {showText && (
+        <TextOverlay>
+          <Text>メールを書く時間すらも惜しんでしまうあなたへ</Text>
+        </TextOverlay>
+      )}
+      {!showText && (
+        <>
+          <Header />
+          <Container>
+            <Text>どこにメールを送るのか選択してね！</Text>
+            <ButtonContainer>
+              <StyledLink to="/university">
+                <Button>大学(例:講義欠席)</Button>
+              </StyledLink>
+              <StyledLink to="/part-time">
+                <Button>バイト(例:バイト欠席、遅刻)</Button>
+              </StyledLink>
+              <StyledLink to="/company">
+                <Button>企業、会社(例:内定、インターン内定辞退)</Button>
+              </StyledLink>
+            </ButtonContainer>
+          </Container>
+        </>
+      )}
+    </WelcomeContainer>
+  );
+};
