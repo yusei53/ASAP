86a5f4d86e867f7ee230fbd3d41c75e2d76e568a
今村悠聖 <yusei@imamurayuushounoMacBook-Air.local>
2023-06-10 01:20:55 +0900

    プロジェクト名変更と、画面遷移時のアニメーション追加

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,35 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export const Header = () => {
   return (
     <SHeader>
-      <SString>連絡マスター</SString>
+      <SString>A S A P</SString>
     </SHeader>
   );
 };
 
 const SHeader = styled.div`
-  background-color: #1c97a0;
+  background: linear-gradient(0deg, #1c97a0, #09a1d7);
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.2);
 `;
 
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateY(-10px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
 const SString = styled.div`
+  font-family: "Montserrat", sans-serif; /* フォントの指定 */
   font-weight: 600;
   text-align: center;
-  line-height: 2;
-  font-size: 50px;
+  line-height: 1.5;
+  font-size: 70px;
   padding: 1% 0;
+  animation: ${fadeIn} 1s ease-in-out; /* アニメーションの適用 */
 `;
